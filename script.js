eaf3005750d5cf9167e82746679c729d698ce5f6
ALWIN K J <AJ-016@users.noreply.github.com>
2024-05-01 15:39:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+var typed = new Typed('.typed', {
+    strings: ['Graphic Designer', 'Web Developer', '3d Artist'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
